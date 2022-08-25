7703820a141f35e4f59a4d74c63f793639a93dde
RNDpacman <dvmn@riseup.net>
2022-08-26 00:48:21 +0300

    some improve main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import requests
 
 
-def get_weather(*cities, lang='en', options='nTqu'):
+def get_weather(*locations, lang='en', options='nTqu'):
     '''
     Печатает погоду переданных городов на консоль
-    opt - https://wttr.in/:help
+    options - https://wttr.in/:help
     '''
     host = 'wttr.in'
     payload = {'lang': lang, **{option: '' for option in options}}
 
-    for city in cities:
-        url = f'https://{host}/{city}'
+    for location in locations:
+        url = f'https://{host}/{location}'
         response = requests.get(url, params=payload)
         response.raise_for_status()
         print('='*80)
@@ -19,9 +19,9 @@ def get_weather(*cities, lang='en', options='nTqu'):
 
 if __name__ == '__main__':
 
-    cities = ('Шереметьево',
-              'Череповец',
-              'Лондон')
+    locations = ('Шереметьево',
+                 'Череповец',
+                 'Лондон')
 
-    get_weather(*cities, lang='ru', options='nTqmM')
+    get_weather(*locations, lang='ru', options='nTqM')
 
